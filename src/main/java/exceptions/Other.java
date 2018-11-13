536b26403f11fdb31ab53918f0f8ab1c4e53418b
Jesper Petersen <info@ordklar.dk>
2018-11-13 12:48:13 +0100

    Add more method to DAO forespørgsel

---
+++ src/main/java/exceptions/Other.java
@@ -0,0 +1,18 @@
+package exceptions;
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author Jespe
+ */
+public class Other extends Exception  {
+    public Other(String message)
+    {
+        super(message);
+    }
+}
