054dbdf114d06ddf705f6fae115144e6e7c19f60
Jesper Petersen <info@ordklar.dk>
2018-11-21 18:53:51 +0100

    Opret DrawingValue + Class til, at løse SVG opgaven

---
+++ src/main/java/jc/fog/presentation/Drawing.java
@@ -0,0 +1,17 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jc.fog.presentation;
+
+import jc.fog.logic.DrawingValue;
+
+public class Drawing {
+    
+    DrawingValue DrawingA = new DrawingValue(5, 9, 10, 200, "000");
+    
+    public static void DrewingSVG(){
+                
+    }
+}
